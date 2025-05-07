3dff3fe6470a9472b6d05ad378621e0a96f08239
DESKTOP-3MH65FG\Joe <xjoeabc3x@gmail.com>
2025-05-07 23:47:40 +0800

    [feat] Handle safeArea and backPressed.

--- app/src/main/java/com/joe/taichungapp/MainActivity.kt
+++ app/src/main/java/com/joe/taichungapp/MainActivity.kt
@@ -1,18 +1,38 @@
 package com.joe.taichungapp
 
-import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.joe.taichungapp.util.LocaleHelper
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
-    override fun attachBaseContext(newBase: Context) {
-        val context = LocaleHelper.updateLocale(newBase, LocaleHelper.getCurrentLanguage())
-        super.attachBaseContext(context)
-    }
+//    override fun attachBaseContext(newBase: Context) {
+//        val context = LocaleHelper.updateLocale(newBase, LocaleHelper.getCurrentLanguage())
+//        super.attachBaseContext(context)
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_main)
+        setContentView(R.layout.activity_main)
+        // 設置全局的 WindowInsets 處理
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.nav_host_fragment)) { view, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.setPadding(0, insets.top, 0, insets.bottom)
+            windowInsets
+        }
+    }
+    // 處理返回鍵
+    override fun onBackPressed() {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        if (!navController.popBackStack()) {
+            super.onBackPressed()
+        }
     }
 }
