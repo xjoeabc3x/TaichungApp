02503a5e021658c6000135634938b03dbb0cd5c6
Joe <JoeHuang@giant.com.tw>
2025-05-08 16:39:05 +0800

    [feat] Add website feature and update fragment navigation.

--- app/src/main/java/com/joe/taichungapp/ui/InfoDetailFragment.kt
+++ app/src/main/java/com/joe/taichungapp/ui/InfoDetailFragment.kt
@@ -1,11 +1,16 @@
 package com.joe.taichungapp.ui
 
+import android.opengl.Visibility
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.joe.taichungapp.R
 
 class InfoDetailFragment : Fragment() {
@@ -25,17 +30,30 @@ class InfoDetailFragment : Fragment() {
         val descriptionTextView: TextView = view.findViewById(R.id.descriptionTextView)
         val addressTextView: TextView = view.findViewById(R.id.addressTextView)
         val otherInfoTextView: TextView = view.findViewById(R.id.otherInfoTextView)
+        val picture: ImageView = view.findViewById(R.id.iv_info_detail_picture)
+        val webButton: Button = view.findViewById(R.id.bt_website)
 
         if (flowerInfo != null) {
             titleTextView.text = flowerInfo.flowerType ?: "-"
             descriptionTextView.text = "觀賞時期: ${flowerInfo.viewingPeriod ?: "-"}"
             addressTextView.text = "地址: ${flowerInfo.address ?: "-"}"
             otherInfoTextView.text = "地點: ${flowerInfo.location ?: "-"}"
+            picture.visibility = View.GONE
+            webButton.visibility = View.INVISIBLE
         } else if (attractionsInfo != null) {
             titleTextView.text = attractionsInfo.nameChinese ?: "-"
             descriptionTextView.text = attractionsInfo.descriptionChinese ?: "-"
             addressTextView.text = "地址: ${attractionsInfo.addressChinese ?: "-"}"
             otherInfoTextView.text = "電話: ${attractionsInfo.servicePhone ?: "-"}"
+            Glide.with(view.context)
+                .load(attractionsInfo.photoUrl1)
+                .into(picture)
+            webButton.setOnClickListener {
+                val action = InfoDetailFragmentDirections.actionInfoDetailFragmentToWebViewFragment(attractionsInfo.websiteUrl)
+                findNavController().navigate(action)
+            }
         }
+
+
     }
 }
